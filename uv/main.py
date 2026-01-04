f92be4c65145dd1a1d1fde23f027c815367fc059
Ptolemy2002 <ptolemy2002@gmail.com>
2026-01-03 23:34:08 -0500

    Updated to use argparse and have a dedicated help file.

--- uv/main.py
+++ uv/main.py
@@ -1,6 +1,32 @@
+import argparse
+
 def main():
     print("Hello, World!")
 
 
 if __name__ == "__main__":
+    man = "No manual available."
+    try:
+        with open("man.txt", "r") as f:
+            man = f.read()
+    except FileNotFoundError:
+        # Just go with the default message
+        pass
+
+    parser = argparse.ArgumentParser(
+        description="Basic uv template script.",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=man
+    )
+
+    """
+    parser.add_argument(
+        "arg",
+        type=str,
+        default="default_value",
+        nargs="?",
+        help="An example argument."
+    )
+    """
+
     main()
